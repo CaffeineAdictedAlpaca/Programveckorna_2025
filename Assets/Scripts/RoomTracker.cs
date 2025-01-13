e7b63115e2697a646d36f60e3b8c18f1bb58fe95
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2025-01-13 13:40:59 +0100

    Scen tracker fixed

--- Assets/Scripts/RoomTracker.cs
+++ Assets/Scripts/RoomTracker.cs
@@ -5,55 +5,63 @@ using UnityEngine.SceneManagement;
 
 public class RoomTracker : MonoBehaviour
 {
-    [SerializeField]
-    private List<string> scenesToShuffle = new List<string>(); // List of scene names to shuffle
-    int nextScene;
+    [Tooltip("List of scene names to scramble. Ensure all scenes are added to the Build Settings.")]
+    public List<string> sceneNames = new List<string>();
 
-    private void Start()
+    [Tooltip("Indices of scenes in the list that should not be scrambled.")]
+    public List<int> fixedIndices = new List<int>();
+
+    public List<string> scrambledScenes;
+
+    void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        // Scramble the scene list while preserving fixed indices.
+        scrambledScenes = ScrambleScenes(sceneNames, fixedIndices);
 
-        if (scenesToShuffle.Count == 0)
+        if (scrambledScenes.Count > 0)
         {
-            Debug.LogWarning("No scenes added to the list!");
-            return;
+
         }
-
-        // Ensure the last scene is always the last
-        string lastScene = scenesToShuffle[scenesToShuffle.Count - 1];
-        scenesToShuffle.RemoveAt(scenesToShuffle.Count - 1);
-
-        // Shuffle the rest of the list
-        ShuffleScenes();
-
-        // Re-add the last scene at the end of the list
-        scenesToShuffle.Add(lastScene);
-    }
-
-    // Shuffle the list using the Fisher-Yates algorithm
-    private void ShuffleScenes()
-    {
-        for (int i = scenesToShuffle.Count - 1; i > 0; i--)
+        else
         {
-            int randomIndex = Random.Range(0, i + 1);
-            string temp = scenesToShuffle[i];
-            scenesToShuffle[i] = scenesToShuffle[randomIndex];
-            scenesToShuffle[randomIndex] = temp;
-        }
-
-        Debug.Log("Scenes shuffled! Order:");
-        foreach (var scene in scenesToShuffle)
-        {
-            Debug.Log(scene);
+            Debug.LogError("Scene list is empty or invalid.");
         }
     }
-
-    // Load the first scene in the shuffled list
     public void LoadNextScene()
     {
-        string firstScene = scenesToShuffle[nextScene];
-        Debug.Log("Loading scene: " + firstScene);
-        SceneManager.LoadScene(firstScene);
-        nextScene++;
+        SceneManager.LoadScene(scrambledScenes[0]);
+    }
+
+    private List<string> ScrambleScenes(List<string> scenes, List<int> fixedIndices)
+    {
+        // Create a copy of the list to avoid modifying the original.
+        List<string> scrambled = new List<string>(scenes);
+
+        // Create a list of indices that can be scrambled.
+        List<int> scrambleIndices = new List<int>();
+        for (int i = 0; i < scenes.Count; i++)
+        {
+            if (!fixedIndices.Contains(i))
+            {
+                scrambleIndices.Add(i);
+            }
+        }
+
+        // Shuffle the indices that can be scrambled.
+        for (int i = 0; i < scrambleIndices.Count; i++)
+        {
+            int randomIndex = Random.Range(i, scrambleIndices.Count);
+
+            // Swap the scenes at the scrambled indices.
+            int tempIndex = scrambleIndices[i];
+            scrambleIndices[i] = scrambleIndices[randomIndex];
+            scrambleIndices[randomIndex] = tempIndex;
+
+            string tempScene = scrambled[scrambleIndices[i]];
+            scrambled[scrambleIndices[i]] = scrambled[scrambleIndices[randomIndex]];
+            scrambled[scrambleIndices[randomIndex]] = tempScene;
+        }
+
+        return scrambled;
     }
 }
