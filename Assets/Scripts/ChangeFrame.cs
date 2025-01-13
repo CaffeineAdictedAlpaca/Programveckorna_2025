e3530c8b1f031197bb10c233bb1f3b4915bddd69
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2025-01-13 16:22:17 +0100

    Stuff

--- Assets/Scripts/ChangeFrame.cs
+++ Assets/Scripts/ChangeFrame.cs
@@ -8,9 +8,15 @@ public class ChangeFrame : MonoBehaviour
     [SerializeField] private List<GameObject> frame = new List<GameObject>();
     [SerializeField] int changeTo;
     [SerializeField] int thisFrame;
+    [SerializeField] bool dissableCamera;
+    [SerializeField] bool disableSword;
+    GameObject sword;
+    CameraScript camera;
 
     private void Start()
     {
+        sword = GameObject.FindGameObjectWithTag("Sword");
+        camera = GameObject.FindAnyObjectByType<CameraScript>().GetComponent<CameraScript>();
         fade = GameObject.FindAnyObjectByType<FadeScript>();
     }
     // Function to call when the object is clicked
@@ -29,6 +35,22 @@ public class ChangeFrame : MonoBehaviour
     {
         fade.StartFade();
         // Wait (adjust duration as needed)
+        if (disableSword)
+        {
+            sword.SetActive(false);
+        }
+        else
+        {
+            sword.SetActive(true);
+        }
+        if (dissableCamera)
+        {
+            camera.enabled = false;
+        }
+        else
+        {
+            camera.enabled = true;
+        }
         yield return new WaitForSeconds(0.4f);
         frame[thisFrame-1].SetActive(false);
         frame[changeTo -1].SetActive(true);
